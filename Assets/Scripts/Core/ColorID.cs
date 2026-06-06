fcb1c6febc777bcc0c7ebfe8f32f7daa23f0661e
Nikhil144bc <posavenkatanikhil123@gmail.com>
2026-06-06 12:59:24 +0530

    Colors Added

--- Assets/Scripts/Core/ColorID.cs
+++ Assets/Scripts/Core/ColorID.cs
@@ -3,6 +3,19 @@ public enum ColorID
     Yellow = 0,
     Pink = 1,
     DarkPink = 2,
-    Purple = 3
-    // Add more colours as you design new levels
+    Purple = 3,
+
+    LightGreen = 4,
+    DarkGreen = 5,
+
+    Cyan = 6,
+
+    LightBlue = 7,
+    Blue = 8,
+
+    Brown = 9,
+
+    Orange = 10,
+
+    Gold = 11
 }
